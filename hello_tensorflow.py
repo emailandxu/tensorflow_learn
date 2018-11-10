7d2d236e286f99511660aa7662c0492ab215775d
emailandxu <emailandxu@qq.com>
2018-11-10 14:56:50 +0800

    use tensor node to be input node

--- hello_tensorflow.py
+++ hello_tensorflow.py
@@ -1,21 +1,20 @@
 import tensorflow as tf
 
-# define the input layer
-a = tf.constant(5,name="input_a")
-b = tf.constant(3,name="input_b")
+# define the input layer as 1-d tensor
+a = tf.constant([5,3],name="input_a")
 
 # define the second layer
-c = tf.multiply(a,b,name="mul_c")
-d = tf.add(a,b,name="add_d")
+# it will recieve all the items of the tensor, 
+# sum them or product them
+b = tf.reduce_prod(a,name="prod_b")
+c = tf.reduce_sum(a,name="sum_c")
 
 # define the output layer
-e = tf.add(c,d,name="add_e")
+d = tf.add(b,c,name="add_d")
 
 with tf.Session() as sess:
-    output = sess.run(e)
+    output = sess.run(d)
     print(output)
 
-    # you can pass any op into sess.run()
-    print(sess.run(c))
     # SummaryWriter is deprecated, use tf.summary.FileWriter
     writer = tf.summary.FileWriter('./my_graph', sess.graph)
